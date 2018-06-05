dd30140d72a40a8b763292f81921bfa819a5ff02
Mosarrof Hossain <phmosharaf@gmail.com>
2018-06-05 16:46:32 -0400

    I just started to solve the home work

---
+++ src/resultSheet/ResultSheet01.java
@@ -0,0 +1,50 @@
+package resultSheet;
+
+public class ResultSheet01 {
+
+	public ResultSheet01() {
+		
+	}
+
+	public static void main(String[] args) {
+	String robi="60";
+	String kobi="80";
+	
+	/*int  A,B,C,D;
+	A = 78;
+	B = 68;
+	C = 58;
+	D = 49;
+	
+	if (A>=75 && A<=100)
+	{
+		System.out.println("Student got Grade A and mark range is (75 - 100), Total mark = "+A);
+	}
+	else if (B>=60 && B<=75)
+	{
+		System.out.println("Grade B and the student's mark range is (65 - 75.");
+	}
+	else if (C>=55 && C<=60)
+		 mnj
+	{
+		System.out.println("Grade C");
+	}
+	else if (D>=55 && D<=60)
+	{
+		System.out.println("Grade D");
+	}	
+	else {
+		System.out.println(" Fail");
+	}*/
+	
+	if(robi=="60")
+	{
+		System.out.println("Robi's grade B");
+	}
+	if else(kobi=="80")
+	{
+		System.out.println("Grade A");
+	}
+	}
+
+}
